84dcbad43c7a9fab2ce30078e47c261ff2a9db39
zen <zenon.deviagge@gmail.com>
2026-03-30 12:19:42 -0300

    fotona done

--- src/components/Fotona/Fotona.tsx
+++ src/components/Fotona/Fotona.tsx
@@ -1,11 +1,31 @@
+import { FunctionComponent } from 'react'
 import styles from './Fotona.module.css'
 
-const Fotona: React.FC = () => {
+const Fotona: FunctionComponent = () => {
   return (
     <section className={styles.fotona} id="fotona">
-      <div className={`container ${styles.fotona__inner}`}>
-        <h2 className={styles.fotona__title}>Fotona</h2>
-        <p className={styles.fotona__subtitle}>Placeholder</p>
+      <div className={styles.fotonaInner}>
+        <div className={styles.columnaIcono}>
+          <img
+            className={styles.iconoFotona}
+            src="/icono-fotona.svg"
+            alt="Icono Láser Fotona"
+          />
+          <div className={styles.txtFotona}>
+            <b className={styles.laserFotona}>Láser Fotona®</b>
+            <b className={styles.bastaDeMiedo}>¡Basta de miedo al torno!</b>
+          </div>
+        </div>
+        <div className={styles.columnaTxt}>
+          <div className={styles.laTecnologiaLaser}>
+            La tecnología láser utiliza luz, por lo que no genera ruidos agudos,
+            vibraciones ni dolor.
+          </div>
+          <div className={styles.casiSinAnestesia}>
+            • Casi sin anestesia
+            <br />• Recuperación más rápida
+          </div>
+        </div>
       </div>
     </section>
   )
