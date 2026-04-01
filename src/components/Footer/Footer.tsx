03ce3a506506f80f768b8187f8db6b45f2467bf5
zen <zenon.deviagge@gmail.com>
2026-04-01 19:39:24 -0300

    fix tratamientos card layout on desktop smaller screens

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,74 @@
+import React from 'react'
 import styles from './Footer.module.css'
 
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={`container ${styles.footer__inner}`}>
-        <p>&copy; 2026 Consultorio. Todos los derechos reservados.</p>
+    <footer className={styles.seccionFooter}>
+      <div className={styles.inner}>
+        <div className={styles.footerColumns}>
+          {/* Logo + accepted payments */}
+          <div className={styles.logoAceptamos}>
+            <img
+              className={styles.logoIcon}
+              src="/icono-marca-reverse.svg"
+              alt="Zenón Ponce Odontología"
+            />
+            <b className={`${styles.aceptamosTodos} ${styles.desktopOnly}`}>
+              <span className={styles.aceptamosTodos2}>ACEPTAMOS TODOS </span>
+              <span className={styles.losMediosDe}>LOS MEDIOS DE PAGO.</span>
+            </b>
+          </div>
+
+          {/* Address + map */}
+          <div className={styles.ubicacion}>
+            <b className={styles.visitanosEnNuestro}>
+              VISITANOS EN NUESTRO CONSULTORIO:
+            </b>
+            <div className={styles.addressText}>
+              Villegas 175, Coronel Suárez, Bs. As.
+            </div>
+            <img className={styles.mapImg} src="" alt="Mapa consultorio" />
+          </div>
+
+          {/* Social + contact + CTA */}
+          <div className={styles.footerRight}>
+            <div className={styles.redesSociales}>
+              <b className={styles.sectionLabel}>REDES SOCIALES</b>
+              <img
+                className={styles.redesSocialesIcon}
+                src="/redes-sociales.svg"
+                alt="Instagram y Facebook"
+              />
+            </div>
+            <div className={styles.contacto}>
+              <b className={styles.sectionLabel}>CONTACTO</b>
+              <div className={styles.whatsapp}>
+                <img
+                  className={styles.iconoWa}
+                  src="/icono-wa.svg"
+                  alt="WhatsApp"
+                />
+                <span className={styles.phone}>2926 47 6050</span>
+              </div>
+            </div>
+            <button className={styles.btnContactanos}>
+              <b className={styles.btnLabel}>CONTACTANOS</b>
+            </button>
+          </div>
+        </div>
+
+        {/* Copyright strip */}
+        <b className={`${styles.aceptamosTodos} ${styles.mobileTabletOnly}`}>
+          <span className={styles.aceptamosTodos2}>ACEPTAMOS TODOS </span>
+          <span className={styles.losMediosDe}>LOS MEDIOS DE PAGO.</span>
+        </b>
+        <div className={styles.copyright}>
+          <div className={styles.copyrightDivider} />
+          <p className={styles.copyrightText}>
+            Copyright &copy; 2026 Zenón Ponce Odontología | Todos los derechos
+            reservados.
+          </p>
+        </div>
       </div>
     </footer>
   )
